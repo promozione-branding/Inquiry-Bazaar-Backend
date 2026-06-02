843497dddfc4f9ff4720bcd6747c38f49591d954
Anshuman <anshutyagi799@gmail.com>
2026-06-02 18:14:43 +0530

    api fix

--- src/modules/users/services/userWebpage.service.js
+++ src/modules/users/services/userWebpage.service.js
@@ -1,5 +1,7 @@
 import Webpage from "../models/userWebpage.model.js";
 import Business from "../models/userBusiness.model.js";
+import Product from "../../products/models/product.model.js";
+import ProductMedia from "../../products/models/productMedia.model.js";
 
 export const getWebpageBySlugService = async (slug) => {
     const webpage = await Webpage.findOne({ slug })
@@ -8,8 +10,18 @@ export const getWebpageBySlugService = async (slug) => {
     if (!webpage) { return null; }
     const business = await Business.findOne({ userId: webpage.userId._id, }).lean();
 
+    const products = await Product.find({ supplierId: webpage.userId._id, })
+        .sort({ createdAt: -1 }).limit(8).lean();
+
+    const productsWithMedia = await Promise.all(products.map(async (product) => {
+        const media = await ProductMedia.find({ productId: product._id, }).sort({ isPrimary: -1 }).lean();
+        return { ...product, media, };
+    })
+    );
+
     return {
         ...webpage,
         user: { ...webpage.userId, business, },
+        products: productsWithMedia,
     };
 };
